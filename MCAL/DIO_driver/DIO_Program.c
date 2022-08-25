902094e53e2ec8e02aa93dd0ae3465ed5e32a8c3
Windows <yousseftarekfawzy986@gmail.com>
2022-08-26 00:35:10 +0200

    Fixed Syntax errors

--- MCAL/DIO_driver/DIO_Program.c
+++ MCAL/DIO_driver/DIO_Program.c
@@ -8,9 +8,10 @@
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 #include "DIO_Register.h"
-#include "DIO_int.h"
 #include "DIO_config.h"
 #include "DIO_private.h"
+#include "DIO_int.h"
+
 
 void DIO_Init(void)
 {
