dc9d6baebe4e2a2c1ea63a9566279f5a152ce174
Windows <yousseftarekfawzy986@gmail.com>
2022-08-26 19:53:53 +0200

    Fixed The Initalization Function WriteCMD instead of WriteData

--- HAL/LCD/LCD_Program.c
+++ HAL/LCD/LCD_Program.c
@@ -12,6 +12,8 @@
 #include "LCD_Private.h"
 #include "LCD_Config.h"
 #include "LCD_Interface.h"
+#define F_CPU 16000000UL
+#include <util/delay.h>
 
 
 static u8 InitStepFinished=0;
@@ -26,21 +28,21 @@ void LCD_VoidInit(void){
 	//4 bit mode 
 	_delay_ms(30);
 	
-	LCD_U8WriteData(Function_SET_Code);
-	LCD_U8WriteData(Function_SET_Code);
-	LCD_U8WriteData(NF);
+	LCD_U8WriteCmd(Function_SET_Code);
+	LCD_U8WriteCmd(Function_SET_Code);
+	LCD_U8WriteCmd(NF);
 	_delay_ms(1);
 	
-	LCD_U8WriteData(Initalize_CODE);
-	LCD_U8WriteData(Display_Config);
+	LCD_U8WriteCmd(Initalize_CODE);
+	LCD_U8WriteCmd(Display_Config);
 	_delay_ms(1);
 	
-	LCD_U8WriteData(Initalize_CODE);
-	LCD_U8WriteData(Display_Clear);
+	LCD_U8WriteCmd(Initalize_CODE);
+	LCD_U8WriteCmd(Display_Clear);
 	_delay_ms(2);
 	
-	LCD_U8WriteData(Initalize_CODE);
-	LCD_U8WriteData(Entry_Mode_Config);
+	LCD_U8WriteCmd(Initalize_CODE);
+	LCD_U8WriteCmd(Entry_Mode_Config);
 	_delay_ms(2);
 
 	
@@ -80,7 +82,7 @@ void LCD_U8WriteCmd(u8 Cmd){
 	
 void LCD_U8WriteData(u8 Data){
 	DIO_VoidSetPinValue ('B',RS,DIO_U8_HIGH ); 
-   DIO_VoidSetPinValue ('B',RW,DIO_U8_LOW );	
+    DIO_VoidSetPinValue ('B',RW,DIO_U8_LOW );	
     
     DIO_VoidSetPinValue ('A',LCD_D4,GET_BIT(Data,4));
     DIO_VoidSetPinValue ('A',LCD_D5,GET_BIT(Data,5) );
@@ -106,3 +108,19 @@ void LCD_U8WriteData(u8 Data){
     DIO_VoidSetPinValue ('B',E,DIO_U8_LOW);
     //execution time
     _delay_ms(2);}
+	
+void LCD_WriteString(u8* Str){
+	u8 index=0;
+	while(Str[index] != '\0'){
+		LCD_U8WriteData(Str[index]);
+	index++;
+	}
+}
+
+/*void LCD_GoTo(u8 Row,u8 Column){
+	u8 address;
+	if(Row<2 && Column<16){
+		address=(Row*0x40)+Column;
+		
+	}
+}*/ // uncompleted
